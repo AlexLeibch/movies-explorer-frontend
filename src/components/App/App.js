970c8b33db16d0c29243995df44af372a930abe9
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-11-25 13:38:45 +0200

    сверстал Promo и Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Header from '../Header/Header';
+import {Route, Switch} from 'react-router-dom'
+import Main from '../Main/Main';
+
 function App() {
   return (
     <div>
-      <Header/>
+      <Switch>
+        <Route exact path="/">
+          <Header bgColor="blue" textColor="white"/>
+          <Main/>
+        </Route>
+        <Route exact to="/movies">
+          <Header bgColor="white" textColor="black"/>
+        </Route>
+        </Switch>
     </div>
 
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Promo from '../Promo/Promo'
+
+function Main() {
+    return (
+        <div>
+            <Promo/>
+        </div>
+    )
+}
+
+export default Main
