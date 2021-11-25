d6305f702aebb16c26cd227aa6f01d2d02024f09
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-11-25 15:14:25 +0200

    сверстать AboutProject

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Promo.css'
 function Promo() {
     return (
-        <div className="promo">
+        <section className="promo">
             <div className="promo__container">
                 <div className="promo__wrapper">
                     <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
@@ -11,8 +11,7 @@ function Promo() {
                 </div>
                 <div className="promo__img"></div>
             </div>
-
-        </div>
+        </section>
     )
 }
 
